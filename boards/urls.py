8fbf9806ed433d8940b4ded864afc5bfc71011af
sarandis <sarandis10@hotmail.com>
2021-07-23 14:16:47 +0100

    got one board works

--- boards/urls.py
+++ boards/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BoardListView
+from .views import BoardListView, BoardOneView
 
 urlpatterns = [
-    path('',BoardListView.as_view())
+    path('',BoardListView.as_view()),
+    path('<int:pk>/', BoardOneView.as_view())
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 
 from .models import Board
 from .serializers import BoardSerializer
@@ -10,3 +11,18 @@ class BoardListView(APIView):
         windsurf = Board.objects.all()
         serialised_windsurfs=BoardSerializer(windsurf,many=True)
         return Response(serialised_windsurfs.data,status = status.HTTP_200_OK)
+
+class BoardOneView(APIView):
+    def check_board_exists(self, pk):
+        try:
+            print("111111")
+            return Board.objects.get(pk=pk)
+        except Board.DoesNotExist:
+            print("333333333")
+            raise NotFound(detail="🆘 Can't find that Board")
+
+    def get(self, _request, pk):
+        print("222222222")
+        windsuf_board = self.check_board_exists(pk=pk)
+        serialized_board = BoardSerializer(windsuf_board)
+        return Response(serialized_board.data, status=status.HTTP_200_OK)
