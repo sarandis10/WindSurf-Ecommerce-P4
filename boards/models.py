6b03fa49b00cca7ac993d7b42551f7851f0e8301
sarandis <sarandis10@hotmail.com>
2021-07-23 13:41:57 +0100

    server setted up

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Board(models.Model):
+    manufacturer = models.CharField(max_length=50, default=None)
+    make = models.CharField(max_length=50, default=None)
+    liters = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+    new = models.BooleanField()
+    image = models.CharField(max_length=500, default=None)
+
+    def __str__ (self):
+        return f"{self.manufacturer}-{self.make}"
