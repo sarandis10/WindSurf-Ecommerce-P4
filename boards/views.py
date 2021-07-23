7ba1590fa8fca908a80f489979da652482ed9d01
sarandis <sarandis10@hotmail.com>
2021-07-23 14:00:38 +0100

    get request works

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Board
+from .serializers import BoardSerializer
+
+class BoardListView(APIView):
+    def get(self,_request):
+        windsurf = Board.objects.all()
+        serialised_windsurfs=BoardSerializer(windsurf,many=True)
+        return Response(serialised_windsurfs.data,status = status.HTTP_200_OK)
