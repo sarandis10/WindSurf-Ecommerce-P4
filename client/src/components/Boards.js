f5a14613b29a16ffb360e76c13f08550b83cf61c
sarandis <sarandis10@hotmail.com>
2021-07-27 08:33:21 +0100

    single filter works

--- client/src/components/Boards.js
+++ client/src/components/Boards.js
@@ -18,15 +18,48 @@ const Boards = () => {
     getData()
   },[])
 
+
+  const [filteredBoards, setFilteredBoards] = useState([])
+
+  const handleFilter = (event) => {
+    const filteredBoardsArray = boards.filter(item => {
+      return item.manufacturer === event.target.value
+    })
+    setFilteredBoards(filteredBoardsArray)
+  }
+
+  const uniqueRegion = []
+
+  // populates my dropdown list
+  boards.map(item => {
+    if (uniqueRegion.indexOf(item.manufacturer) === -1) {
+      uniqueRegion.push(item.manufacturer)
+    }
+  })
+
+  console.log('filteredBoards',filteredBoards)
+  console.log('uniqueRegion',uniqueRegion)
   return (
     <>
-    
-      <div className="card" styles="width: 18rem;">
-        {boards.map(iter=> {
+      <select onChange={handleFilter}>{uniqueRegion.map((item) => {
+        if (item === '') {
+          return <option key='none' value=''>Other</option>
+        } else {
+          return (
+            <>
+              <option key={item.index} value={item}>{item}</option>
+            </>
+          )
+        }
+      })}
+      <option key='last' value="All">All</option>
+      </select>
+      <div onChange={handleFilter} className="card" styles="width: 18rem;">
+        {(filteredBoards.length > 0 ? filteredBoards : boards).map((iter,index)=> {
           return (
             <>
               <img src={iter.image} className="card-img-top" alt="..."/>
-              <div className="card-body">
+              <div key ={index} className="card-body">
                 <h5 className="card-title">Make: {iter.manufacturer}</h5>
                 <p className="card-text">Model: {iter.make}</p>
                 <a href="#" className="btn btn-primary">Find More</a>
