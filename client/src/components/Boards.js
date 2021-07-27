d5df3cc166a382c4d6277ef71c6cc27f7337e6ed
sarandis <sarandis10@hotmail.com>
2021-07-27 17:41:50 +0100

    before the rfactoring

--- client/src/components/Boards.js
+++ client/src/components/Boards.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 
 const Boards = () => {
@@ -20,14 +21,25 @@ const Boards = () => {
 
 
   const [filteredBoards, setFilteredBoards] = useState([])
+  const [filteredBoardsbymake, setFilteredBoardstbymake] = useState([])
 
   const handleFilter = (event) => {
-    const filteredBoardsArray = boards.filter(item => {
+    const filteredBoardsArray = (filteredBoardsbymake.length > 0 ? filteredBoardsbymake : boards).filter(item => {
       return item.manufacturer === event.target.value
     })
     setFilteredBoards(filteredBoardsArray)
+    // setFilteredBoardstbymake(filteredBoardsArray)
   }
+  
 
+  const handleFilterMake = (event) => {
+    const filteredBoardsArray = (filteredBoards.length > 0 ? filteredBoards : boards).filter(item => {
+      return item.make === event.target.value
+    })
+    // setFilteredBoards(filteredBoardsArray)
+    setFilteredBoardstbymake(filteredBoardsArray)
+  }
+  console.log('giati den exeis data mesa',filteredBoardsbymake)
   const uniqueRegion = []
 
   // populates my dropdown list
@@ -37,8 +49,19 @@ const Boards = () => {
     }
   })
 
+
+  // experiment
+  const uniqueType = []
+  boards.map(item => {
+    if (uniqueType.indexOf(item.make) === -1) {
+      uniqueType.push(item.make)
+    }
+  })
+
   console.log('filteredBoards',filteredBoards)
   console.log('uniqueRegion',uniqueRegion)
+  console.log('uniqueType',uniqueType)
+
   return (
     <>
       <select onChange={handleFilter}>{uniqueRegion.map((item) => {
@@ -54,6 +77,21 @@ const Boards = () => {
       })}
       <option key='last' value="All">All</option>
       </select>
+
+      <select onChange={handleFilterMake}>{uniqueType.map((item) => {
+        if (item === '') {
+          return <option key='none' value=''>Other</option>
+        } else {
+          return (
+            <>
+              <option key={item.index} value={item}>{item}</option>
+            </>
+          )
+        }
+      })}
+      <option key='last' value="All">All</option>
+      </select>
+
       <div onChange={handleFilter} className="card" styles="width: 18rem;">
         {(filteredBoards.length > 0 ? filteredBoards : boards).map((iter,index)=> {
           return (
@@ -62,7 +100,9 @@ const Boards = () => {
               <div key ={index} className="card-body">
                 <h5 className="card-title">Make: {iter.manufacturer}</h5>
                 <p className="card-text">Model: {iter.make}</p>
-                <a href="#" className="btn btn-primary">Find More</a>
+                <Link >
+                  <a href="#" className="btn btn-primary">Find More</a>
+                </Link>
               </div>
             </>
           )
