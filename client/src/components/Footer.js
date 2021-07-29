e42e278e82980e4e1d29e5e89e60231c73a03e1e
sarandis <sarandis10@hotmail.com>
2021-07-29 18:08:54 +0100

    Final Submition

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
 
 const Footer = () => {
 
@@ -8,10 +8,6 @@ const Footer = () => {
       <footer className="text-center text-lg-start bg-light text-muted">
         <section
           className="d-flex justify-content-center align-items-center justify-content-lg-end p-5 border-bottom fw-bold">
-          <div className="me-5 d-none d-lg-block">
-            <span>Get connected with us on social networks:</span>
-          </div>
-
           <div>
             <a href="" className="me-4 text-reset">
               {/* <img src={facebook} width="36" height="36" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16"></img> */}
@@ -62,7 +58,7 @@ const Footer = () => {
         </section>
         <div className="text-center p-4" styles="background-color: rgba(0, 0, 0, 0.05);">
         © 2021 Copyright:
-          <a className="text-reset fw-bold" href="https://mdbootstrap.com/"> CEST-DevelopmentTeam</a>
+          <a className="text-reset fw-bold" href="https://mdbootstrap.com/"> Sarantis Development</a>
         </div>
 
       </footer>
